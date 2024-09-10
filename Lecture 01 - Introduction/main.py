80434a7ab71ca9a18b11839764bd808ef4d6f3b4
IrakliTabatadze <irakli.tabatadz.443@gmail.com>
2024-09-10 22:22:04 +0400

    Lecture 1

---
+++ Lecture 01 - Introduction/main.py
@@ -0,0 +1,52 @@
+#################################################
+# Comment
+#################################################
+
+# Hello World
+
+'''
+sdfasdf
+asdfasdfasdf
+asdfasdf
+asdf
+'''
+
+
+
+###########################################################
+# Variables
+###########################################################
+
+number = 10
+float_number = -5.5
+txt = 'I\'m some new text'
+txt2 = "I'm some new text"
+boolean = True
+boolean2 = False
+
+###########################################################
+# Input/Output
+###########################################################
+
+print("Hello Everyone")
+print(number)
+print(float_number)
+print(txt)
+print(txt2)
+print(boolean)
+print(boolean2)
+
+
+name = input("Please Input Your Name: ")
+print(name)
+
+
+
+number1 = float(input("Please enter a number: "))
+number2 = int(input("Please enter a number: "))
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 ** number2)
